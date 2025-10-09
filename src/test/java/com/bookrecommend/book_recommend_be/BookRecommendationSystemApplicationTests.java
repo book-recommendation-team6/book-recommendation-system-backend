0dbf4d0c4cd2b827975b60df054ddd7956d55ff5
tphuc263 <tphuc.ptit@gmail.com>
2025-10-09 20:14:55 +0700

    init proj

---
+++ src/test/java/com/bookrecommend/book_recommend_be/BookRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookrecommend.book_recommend_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecommendationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
