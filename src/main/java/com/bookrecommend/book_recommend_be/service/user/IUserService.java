1bd0c87af756ef5ed3f583d3c35c7e34dcfe937d
DUYBEGINER <phucduygg8485@gmail.com>
2025-10-29 13:48:45 +0700

    push before mêrge

--- src/main/java/com/bookrecommend/book_recommend_be/service/user/IUserService.java
+++ src/main/java/com/bookrecommend/book_recommend_be/service/user/IUserService.java
@@ -4,6 +4,7 @@ import com.bookrecommend.book_recommend_be.dto.request.UpdateUserRequest;
 import com.bookrecommend.book_recommend_be.dto.response.UserResponse;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IUserService {
     Page<UserResponse> getAllUsers(int page, int size);
